175c85a55f85998c940f19210e4a5877182d3514
yeony612 <77176550+yeony612@users.noreply.github.com>
2022-08-14 23:43:33 -0400

    update website

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -5,9 +5,22 @@ import axios from "axios";
 
 const InfoCard = () => {
   const text = "Go Home";
+
+  const [fact, setFact] = useState([]);
+  useEffect(() => {
+    const getFact = async () => {
+      const response = await axios.get(
+        "https://zoo-animal-api.herokuapp.com/animals/rand"
+      );
+      console.log(response.data.diet);
+      setFact(response.data.diet);
+    };
+    getFact();
+  }, []);
   return (
     <div className="info-card">
       <h1>Dog Fact</h1>
+      <p>{fact}</p>
       <Link to="/">
         <Button text={text} />
       </Link>
