d2eafaf58f356e6b5eabc1d081106e6170943eaf
yeony612 <77176550+yeony612@users.noreply.github.com>
2022-08-18 12:27:53 -0400

    fixed error

--- src/pages/info.js
+++ src/pages/info.js
@@ -4,16 +4,16 @@ import axios from "axios";
 import InfoCard from "../components/InfoCard";
 
 const Info = () => {
-  const [image, setImage] = useState([]);
-  useEffect(() => {
-    const getImages = async () => {
-      const response = await axios.get(
-        "https://dog.ceo/api/breeds/image/random/15"
-      );
-      setImage(response.data.message);
-    };
-    getImages();
-  }, []);
+  // const [image, setImage] = useState([]);
+  // useEffect(() => {
+  //   const getImages = async () => {
+  //     const response = await axios.get(
+  //       "https://dog.ceo/api/breeds/image/random/15"
+  //     );
+  //     setImage(response.data.message);
+  //   };
+  //   getImages();
+  // }, []);
 
   return <InfoCard />;
 };
